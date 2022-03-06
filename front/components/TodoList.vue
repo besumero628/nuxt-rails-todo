f20b69c642788a1c3bb6bd0d3b5e536159925d45
besumero628 <besumero628@gmail.com>
2022-03-06 15:28:21 +0900

    fix TodoList

--- front/components/TodoList.vue
+++ front/components/TodoList.vue
@@ -3,7 +3,7 @@
     <v-card-title>
       Todo List
       <v-spacer></v-spacer>
-      <v-text-field v-model="search" label="Search" single-line, hide-details></v-text-field>
+      <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
     </v-card-title>
     <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
   </v-card>
@@ -22,14 +22,13 @@ export default {
           sortable: false,
           value: "title"
         },
-        {
-          text: "ユーザー名",
+        { text: "ユーザー名", 
           value: "username"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style>
